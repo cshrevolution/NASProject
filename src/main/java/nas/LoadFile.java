c8d55061d1c67874649dc6ced0a1656930322c04
csh_revolution <csh_revolution@localhost>
2025-06-07 10:18:12 +0900

    202506071018

--- src/main/java/nas/LoadFile.java
+++ src/main/java/nas/LoadFile.java
@@ -69,9 +69,12 @@ public class LoadFile extends HttpServlet {
 		    		out.println("<script>alert('잘못된 접근입니다!'); location.href = '/';</script>");
 		    }
 		    
+		    //파일 버튼 누를 시 기존 동작 (GET Method)
+		    /*
 		    if (request.getParameter("dir") != null) {
     			currentDirectory = request.getParameter("dir");
     		}
+    		*/
 		    
 		    File[] dirs = new File(currentDirectory).listFiles();
 		    
@@ -115,82 +118,41 @@ public class LoadFile extends HttpServlet {
 	        }
 		}	
 	}
-	/*
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
-		
-		HttpSession session = request.getSession();
+	
 		PrintWriter out = response.getWriter();
+		String currentDirectory = null;
 		
 		try {
-			ctx = new InitialContext();
-		    ds = (DataSource) ctx.lookup("datasource1");
-		    con = ds.getConnection();
-		    String currentDirectory = null;
-		    
-		    if (session.getAttribute("UID") == null) {
-		    		out.println("<script>alert('잘못된 접근입니다!'); location.href = '/';</script>");
-		    } else {
-		    		uid = Integer.parseInt(session.getAttribute("UID").toString());
-		    }
-		    
-		    ps = con.prepareStatement(sql);
-		    ps.setString(1, Integer.toString(uid));
-		    rs = ps.executeQuery();
-		    
-		    if (rs.next()) {
-		    		currentDirectory = rs.getString(1);
-		    		if (request.getAttribute("dir") != null) {
-		    			currentDirectory = request.getAttribute("dir").toString();
-		    		}
-		    }
-		    else {
-		    		out.println("<script>alert('잘못된 접근입니다!'); location.href = '/';</script>");
-		    }
-		    
-		    File[] dirs = new File(currentDirectory).listFiles();
-		    
-		    if (dirs != null) {
-		    		String[] fileArray = new String[dirs.length];
-		    		
-		    		for (int i = 0; i < dirs.length; i++) {
-		    			fileArray[i] = dirs[i].getAbsolutePath();
-		    		}
-		    		
-		    		Arrays.sort(fileArray, String.CASE_INSENSITIVE_ORDER);
-		    		request.setAttribute("sortedFiles", fileArray);
-		    }
-		    
-		    request.setAttribute("currentDirectory", currentDirectory);
-		    request.getRequestDispatcher("file.jsp").forward(request, response);
+			currentDirectory = request.getParameter("dir").toString();
 			
+			if (currentDirectory.isEmpty()) {
+				out.println("<script>alert('잘못된 접근입니다!'); location.href = '/';</script>");
+			}
+			
+			File[] dirs = new File(currentDirectory).listFiles();
+			
+			if (dirs != null) {
+				String[] fileArray = new String[dirs.length];
+				
+				for (int i = 0; i < dirs.length; i++) {
+					fileArray[i] = dirs[i].getAbsolutePath();
+				}
+				
+				Arrays.sort(fileArray, String.CASE_INSENSITIVE_ORDER);
+				request.setAttribute("sortedFiles", fileArray);
+			}
+			
+			request.setAttribute("currentDirectory", currentDirectory);
+			request.getRequestDispatcher("file.jsp").forward(request, response);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-	        }
-	        if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-	        }
-	        if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-	        }
-		}	
+		}
+		
 	}
-	*/
+
 }
